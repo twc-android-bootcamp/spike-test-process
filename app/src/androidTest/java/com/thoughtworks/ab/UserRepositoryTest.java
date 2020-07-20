1d10e1c5d29aa9ded174f731a35ffad6cd819543
Yuan Shenjian <sjyuan@thoughtworks.com>
2020-07-20 19:13:51 +0800

    Test with RxJava
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

--- app/src/androidTest/java/com/thoughtworks/ab/UserRepositoryTest.java
+++ app/src/androidTest/java/com/thoughtworks/ab/UserRepositoryTest.java
@@ -1,33 +1,33 @@
 
 package com.thoughtworks.ab;
 
-import android.content.Context;
-
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.room.Room;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.thoughtworks.ab.repository.AppDatabase;
+import com.thoughtworks.ab.repository.UserRepositoryImpl;
+import com.thoughtworks.ab.repository.entity.User;
 import com.thoughtworks.ab.view.MainActivity;
 import com.thoughtworks.ab.viewmodel.UserRepository;
 import com.thoughtworks.ab.viewmodel.UserVO;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.Random;
+import java.util.function.Predicate;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.notNullValue;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -35,16 +35,34 @@ public class UserRepositoryTest {
 
     @Rule
     public ActivityTestRule mActivityRule = new ActivityTestRule(MainActivity.class);
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    private AppDatabase appDatabase;
+
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() {
+        appDatabase = Room.inMemoryDatabaseBuilder(
+                InstrumentationRegistry.getInstrumentation().getTargetContext(),
+                AppDatabase.class).build();
+        userRepository = new UserRepositoryImpl(appDatabase.userDBDataSource());
+    }
+
+    @After
+    public void tearDown() {
+        appDatabase.close();
+    }
 
     @Test
     public void should_find_correct_user() {
-        MainApplication application = (MainApplication) InstrumentationRegistry.getInstrumentation().getTargetContext().getApplicationContext();
-        UserRepository userRepository = application.userRepository();
         UserVO userVO = new UserVO();
         userVO.setName(new Random().nextInt() + "");
         userVO.setLastName("Yuan");
         userVO.setId("12345");
-        userRepository.save(userVO);
-        assertThat(userRepository.find("12345").toString(), is(userVO.toString()));
+        appDatabase.userDBDataSource().save(userVO.toModel()).subscribeOn(Schedulers.io()).subscribe();
+        userRepository.find("12345").test()
+                .assertValue(user -> user.toString().equals(userVO.toModel().toString()));
     }
 }
